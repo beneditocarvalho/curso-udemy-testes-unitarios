18f1b993d6520da7c2d6254f73f4401a2bb8fcef
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-09-25 20:42:46 -0300

    Primeiro Teste Criado (cenario / acao / verificacao)

---
+++ src/test/java/servicos/LocacaoServiceTest.java
@@ -0,0 +1,32 @@
+package servicos;
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.servicos.LocacaoService;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoServiceTest {
+	
+	@Test
+	public void teste() {
+		
+		//cenario
+		LocacaoService teste = new LocacaoService();
+		 
+		Usuario usuario = new Usuario("Benedito");
+		Filme filme = new Filme("A volta dos que não foram", 3, 20.0);
+		
+		//acao
+		Locacao locacao = teste.alugarFilme(usuario, filme);
+		
+		//verificacao
+		Assert.assertTrue(locacao.getValor() == 20.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+	}
+}
