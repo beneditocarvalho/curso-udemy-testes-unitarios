eadb3b06fe15f6ed221041dbfb6d8ba04ea62a87
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-09-26 00:57:57 -0300

    JUnit - AssertThat

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,32 +1,36 @@
 package br.ce.wcaquino.servicos;
+
+import static org.hamcrest.CoreMatchers.is;
+
 import java.util.Date;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.servicos.LocacaoService;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
-	
+
 	@Test
 	public void teste() {
-		
-		//cenario
+
+		// cenario
 		LocacaoService teste = new LocacaoService();
-		 
+
 		Usuario usuario = new Usuario("Benedito");
 		Filme filme = new Filme("A volta dos que não foram", 3, 20.0);
-		
-		//acao
+
+		// acao
 		Locacao locacao = teste.alugarFilme(usuario, filme);
-		
-		//verificacao
-		Assert.assertEquals(20.0, locacao.getValor(), 0.01);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+
+		// verificacao
+		Assert.assertThat(locacao.getValor(), CoreMatchers.is(20.0));
+		Assert.assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.not(5.0)));
+		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
+		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
 	}
 }
